2cbf88595203db80e61417d4b721e91013a9a01c
muhammad-ridwan576 <mhdr7682@gmail.com>
2025-05-12 20:31:45 +0700

    Pesan kamar

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -1,2 +1,10 @@
-<h1>Welcome : {{ auth()->user()->name }}</h1>
+
+
+@extends('layouts.app2')
+
+@section('content')
+  <h1>Welcome : {{ auth()->user()->name }}</h1>
 <a href="{{ route('logout') }}">Logout</a>
+  {{-- Tambahkan grafik, statistik, dsb --}}
+@endsection
+
