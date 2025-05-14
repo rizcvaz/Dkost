b9cd410d2c30ba7ab675c11820551ffbe17aee26
kawe44 <kwrrq11@gmail.com>
2025-05-14 12:17:43 +0700

    edit dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -3,5 +3,6 @@
 @section('content')
   <h1 class="header">Dashboard Admin</h1>
   <p class="header">Selamat datang, Admin!</p>
+  <p>Anda dapat mengelola data kos</p>
   {{-- Tambahkan grafik, statistik, dsb --}}
 @endsection
